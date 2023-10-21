49ed979bf1f2338ee88deadc5773b77fbadd730f
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-10-21 13:33:10 -0500

    TODO: Turn bot into a service

--- src/noob/config.clj
+++ src/noob/config.clj
@@ -4,3 +4,5 @@
 (def token (env/env "DISCORD_TOKEN"))
 (def app-id (env/env "DISCORD_APP_ID"))
 (def dev-guild (env/env "DISCORD_DEV_GUILD"))
+(def environment (env/env "ENVIRONMENT"))
+(def local? (= "local" environment))
--- src/noob/main.clj
+++ src/noob/main.clj
@@ -2,6 +2,7 @@
   (:require [c3kit.apron.app :as app]
             [c3kit.apron.legend :as legend]
             [c3kit.apron.log :as log]
+            [c3kit.apron.util :as util]
             [c3kit.bucket.db :as db]
             [discljord.events :as discord-events]
             [noob.bot :as bot]
@@ -23,10 +24,25 @@
       (catch Exception e
         (log/error e)))))
 
+(def all-services [db/service])
+(def refresh-services [])
+(defn start-all [] (app/start! all-services))
+(defn stop-all [] (app/stop! all-services))
+
+(defn maybe-init-dev []
+  (when config/local?
+    (let [refresh-init (util/resolve-var 'c3kit.apron.refresh/init)]
+      (refresh-init refresh-services "noob" ['noob.main]))))
+
 (defn -main [& _]
+  ;; TODO [BAC]: turn bot into a service
   (try
     (legend/init! schema.full/legend)
-    (start-db)
+    (maybe-init-dev)
+    (start-all)
     (bot/init! config/token)
     (discord-events/message-pump! (bot/events) (wrap-error events/handle-event))
-    (finally (bot/stop!))))
+    (finally (bot/stop!)))
+  ;(.addShutdownHook (Runtime/getRuntime) (Thread. stop-all))
+  ;(.addShutdownHook (Runtime/getRuntime) (Thread. shutdown-agents))
+  )
