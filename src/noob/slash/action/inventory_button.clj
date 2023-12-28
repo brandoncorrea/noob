c1f066d1ac00423e2f5cff08b2cf03f753cbc1a3
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-12-28 14:55:51 -0500

    Refactor

--- src/noob/slash/action/inventory_button.clj
+++ src/noob/slash/action/inventory_button.clj
@@ -8,6 +8,11 @@
 (defn ->product [request]
   (->> request :data :custom-id (re-find #"\d+") schema/->int product/entity))
 
+(defn toggle-item! [toggle! request user product message]
+  (toggle! user product)
+  ;; TODO [BAC]: Update original message with new buttons rather than sending an ephemeral message
+  (interaction/reply-ephemeral! request message))
+
 (defmethod slash/handle-action "inventory-button" [request]
   (let [user    (user/current request)
         product (->product request)]
@@ -15,14 +20,10 @@
       (nil? product)
       (interaction/reply-ephemeral! request "This item does not exist.")
 
-      ;; TODO [BAC]: Update original message with new buttons rather than sending an ephemeral message
       (user/equipped? user product)
-      (do (user/unequip! user product)
-          (interaction/reply-ephemeral! request "Item unequipped!"))
+      (toggle-item! user/unequip! request user product "Item unequipped!")
 
-      ;; TODO [BAC]: Update original message with new buttons rather than sending an ephemeral message
       (user/owns? user product)
-      (do (user/equip! user (product/entity product))
-          (interaction/reply-ephemeral! request "Item equipped!"))
+      (toggle-item! user/equip! request user product "Item equipped!")
 
       :else (interaction/reply-ephemeral! request "You do not own this item."))))
